f59a26ec1a405a63b990bd23ab113b4b35e23e91
Eder Fonseca <ederluisf@hotmail.com>
2018-02-13 16:55:20 -0200

    Commitando antes de inserir a página de Login

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,45 @@
 <template>
   <div id="app">
-    
     <v-app id="sgf-root">
     
-    <v-navigation-drawer fixed :clipped="$vuetify.breakpoint.mdAndUp" app v-model="drawer" >
+    <v-navigation-drawer class="nav-drawer" fixed :clipped="$vuetify.breakpoint.mdAndUp" app v-model="drawer" :mini-variant.sync="miniDrawer">
       <v-list dense>
-        <template v-for="item in items">
-          <v-layout row v-if="item.heading" align-center :key="item.heading" >
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-            <v-flex xs6 class="text-xs-center">
-              <a href="#!" class="body-2 black--text">EDIT</a>
-            </v-flex>
-          </v-layout>
-          <v-list-group v-else-if="item.children" v-model="item.model" :key="item.text"
-                        :prepend-icon="item.model ? item.icon : item['icon-alt']" append-icon="">
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile v-for="(child, i) in item.children" :key="i" @click="">
-              <v-list-tile-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ child.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-group>
-          
-          <v-list-tile v-else @click="" :key="item.text">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }} </v-icon>
-            </v-list-tile-action>
+        <v-list-group v-for="item in items" :key="item.title" :prepend-icon="item.action" no-action>
+          <v-list-tile slot="activator">
             <v-list-tile-content>
-              <v-list-tile-title>
-                {{ item.text }}
-              </v-list-tile-title>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
-        </template>
+          <v-list-tile v-for="subItem in item.items" :key="subItem.title" @click="goTo(item.title)">
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-icon>{{ subItem.action }}</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
       </v-list>
-      
-      <div class="navbar-header">
-        <router-link class="navbar-brand" to="/home">Home</router-link>
-      </div>
-      
-      <div class="navbar-header">
-        <router-link class="navbar-brand" to="/hello">Hello</router-link>
-      </div>
     </v-navigation-drawer>
     
-    <v-toolbar color="blue darken-3" dark app fixed
-               :clipped-left="$vuetify.breakpoint.mdAndUp">
-      <v-toolbar-title style="width: 300px" class="ml-0 text-md-left">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar color="blue darken-3" dark app fixed :clipped-left="$vuetify.breakpoint.mdAndUp">
+   
+      <v-toolbar-title class="ml-0 text-md-left text-sm-left text-xs-left nav-drawer">
+        <v-toolbar-side-icon @click.stop="$vuetify.breakpoint.mdAndUp ? miniDrawer = !miniDrawer : drawer = !drawer"></v-toolbar-side-icon>
         <span class="hidden-sm-and-down">SGF</span>
       </v-toolbar-title>
+   
       <v-text-field flat solo-inverted prepend-icon="search" label="Pesquisar" class="hidden-sm-and-down"></v-text-field>
       <v-spacer></v-spacer>
+   
       <v-btn icon>
         <v-icon>apps</v-icon>
       </v-btn>
+   
       <v-btn icon>
         <v-icon>notifications</v-icon>
       </v-btn>
+   
       <v-btn icon large>
         <v-avatar size="32px" tile>
           <img src="https://vuetifyjs.com/static/doc-images/logo.svg"
@@ -122,54 +89,68 @@
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
     dialog: false,
+    miniDrawer: false,
     drawer: null,
     items: [
-      { icon: 'contacts', text: 'Contacts' },
-      { icon: 'history', text: 'Frequently contacted' },
-      { icon: 'content_copy', text: 'Duplicates' },
       {
-        icon: 'keyboard_arrow_up',
-        'icon-alt': 'keyboard_arrow_down',
-        text: 'Labels',
-        model: true,
-        children: [{ icon: 'add', text: 'Create label' }]
+        action: "local_activity",
+        title: "Attractions",
+        items: [{ title: "List Item" }]
       },
       {
-        icon: 'keyboard_arrow_up',
-        'icon-alt': 'keyboard_arrow_down',
-        text: 'More',
-        model: false,
-        children: [
-          { text: 'Import' },
-          { text: 'Export' },
-          { text: 'Print' },
-          { text: 'Undo changes' },
-          { text: 'Other contacts' }
+        action: "restaurant",
+        title: "Dining",
+        active: true,
+        items: [
+          { title: "Breakfast & brunch" },
+          { title: "New American" },
+          { title: "Sushi" }
         ]
       },
-      { icon: 'settings', text: 'Settings' },
-      { icon: 'chat_bubble', text: 'Send feedback' },
-      { icon: 'help', text: 'Help' },
-      { icon: 'phonelink', text: 'App downloads' },
-      { icon: 'keyboard', text: 'Go to the old version' }
+      {
+        action: "school",
+        title: "Education"
+      },
+      {
+        action: "directions_run",
+        title: "Family",
+        items: [{ title: "List Item" }]
+      },
+      {
+        action: "healing",
+        title: "Health",
+        items: [{ title: "List Item" }]
+      },
+      {
+        action: "content_cut",
+        title: "Office",
+        items: [{ title: "List Item" }]
+      },
+      {
+        action: "local_offer",
+        title: "Promotions",
+        items: [{ title: "List Item" }]
+      }
     ]
   }),
+
   props: {
     source: String
+  },
+
+  methods: {
+    goTo(path) {
+      this.$router.push({ name: "HelloWorld" });
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .nav-drawer {
+    width: 250px !important;
+  }
 </style>
