1b21ab70b6c183af0202adb6a9b45a46ee70375f
Eder Fonseca <ederluisf@hotmail.com>
2018-03-01 21:50:09 -0300

    Retirando vue-clip porque não tem CSS

--- src/components/manufacturer/ManufacturerForm.vue
+++ src/components/manufacturer/ManufacturerForm.vue
@@ -2,30 +2,6 @@
   <form>
     <v-text-field label="Name"></v-text-field>
 
-    <vue-clip :options="options">
-      <template slot="clip-uploader-action">
-        <div>
-          <div class="dz-message"><h2> Click or Drag and Drop files here upload </h2></div>
-        </div>
-      </template>
-
-      <template slot="clip-uploader-body" slot-scope="props">
-        <div class="uploader-files" v-for="file in props.files" :key="file.name">
-          <img class="file-avatarw" v-bind:src="file.dataUrl" />
-          {{ file.name }} {{ file.status }}
-        </div>
-      </template>
-    </vue-clip>
-
-    <vue-clip :options="options">
-      <template slot="clip-uploader-action" slot-scope="params">
-       <div v-bind:class="{'is-dragging': params.dragging}" class="upload-action">
-          <h2> Click or Drag and Drop files here upload </h2>
-        </div>
-      </template>
-
-    </vue-clip>
-
   </form>
 </template>
 
