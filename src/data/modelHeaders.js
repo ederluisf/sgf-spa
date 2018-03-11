b76159627f7a1791fa5e76e705c53461f5271392
Eder Fonseca <ederluisf@hotmail.com>
2018-03-11 05:31:37 -0300

    New headers

---
+++ src/data/modelHeaders.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, text: 'Montadora', value: 'manufacturer.name', left: true },
+  { id: 2, text: 'Nome', value: 'name', left: true },
+  { id: 3, text: 'Motor', value: 'engine', left: true },
+  { id: 4, text: 'Combustível', value: 'fuel', left: true }
+]
---
+++ src/data/vehicleHeaders.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, text: 'Model', value: 'model.nome', left: true },
+  { id: 2, text: 'Ano', value: 'name', left: true },
+  { id: 3, text: 'Cor', value: 'engine', left: true },
+  { id: 4, text: 'Placa', value: 'plate', left: true }
+]
