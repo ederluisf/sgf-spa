762c13b1136608cd081fc670ed9bd3470ce107d3
Eder Fonseca <ederluisf@hotmail.com>
2018-03-12 13:52:54 -0300

    Pequena alteração no domínio

--- src/data/modelHeaders.js
+++ src/data/modelHeaders.js
@@ -1,6 +1,5 @@
 export default [
   { id: 1, text: 'Montadora', value: 'manufacturer.name', left: true },
   { id: 2, text: 'Nome', value: 'name', left: true },
-  { id: 3, text: 'Motor', value: 'engine', left: true },
-  { id: 4, text: 'Combustível', value: 'fuel', left: true }
+  { id: 3, text: 'Versão', value: 'version', left: true }
 ]
