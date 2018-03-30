b83d7c790933174c12a1f2e38ec0298ceef2f6ef
Eder Fonseca <ederluisf@hotmail.com>
2018-03-30 19:03:19 -0300

    alinhamento e atualização do vuetify

--- src/data/engineType.js
+++ src/data/engineType.js
@@ -1,8 +1,7 @@
 export default [
-    { id: 10, type: 1.0 },
-    { id: 14, type: 1.4 },
-    { id: 16, type: 1.6 },
-    { id: 18, type: 1.8 },
-    { id: 20, type: 2.0 }
-  ]
-  
+  { id: 10, type: 1.0 },
+  { id: 14, type: 1.4 },
+  { id: 16, type: 1.6 },
+  { id: 18, type: 1.8 },
+  { id: 20, type: 2.0 }
+]
